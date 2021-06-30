89a7f990c09b4b12145aff84bae452bcb56100fc
wedfdsf <fds@fdas.asd>
2021-06-30 09:58:19 +0300

    add color panel

--- SuperApp/Form1.cs
+++ SuperApp/Form1.cs
@@ -1,20 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SuperApp
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+            colors = new Color[4];
+            colors[0] = Color.Yellow;
+            colors[1] = Color.Red;
+            colors[2] = Color.Yellow;
+            colors[3] = Color.Green;
+
+            color_iter = -1;
         }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.BackColor = colors[(++color_iter) % colors.Length];
+        }
+        private Color[] colors;
+        private short color_iter;
     }
 }
