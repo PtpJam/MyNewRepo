ba7a2385cb75030fa5a5d5c22a563eabdd4cedc1
wedfdsf <fds@fdas.asd>
2021-07-01 09:44:59 +0300

    Add manager form

--- SuperApp/Form1.Designer.cs
+++ SuperApp/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(305, 214);
+            this.button1.Location = new System.Drawing.Point(12, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +45,7 @@
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(305, 108);
+            this.panel1.Location = new System.Drawing.Point(12, 31);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
             this.panel1.TabIndex = 1;
@@ -55,7 +55,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(379, 80);
+            this.label1.Location = new System.Drawing.Point(86, 3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(18, 25);
             this.label1.TabIndex = 2;
@@ -66,7 +66,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(235, 175);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
